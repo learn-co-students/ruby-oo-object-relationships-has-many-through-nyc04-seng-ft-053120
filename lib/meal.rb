c43853ca3813d13004b119d52dd109017d46e099
github username <github email address>
2020-06-25 15:29:47 -0400

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,35 @@
 class Meal
+    attr_accessor :waiter, :customer, :total, :tip
 
+    @@all = []
+
+    def initialize(waiter, customer, total, tip)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self
+    end
+
+    def self.get_meals_by_customer(customer)
+        @@all.select{ |meal| meal.customer == customer }
+    end
+
+    def self.get_meals_by_waiter(waiter)
+        @@all.select{ |meal| meal.waiter == waiter }
+    end
+
+    def self.get_waiters_by_customer(customer)
+        waiters = []
+        @@all.each do |meal| 
+            if meal.customer == customer
+                waiters << meal.waiter
+            end
+        end
+        waiters
+    end
+
+    def self.all
+        @@all
+    end
 end
