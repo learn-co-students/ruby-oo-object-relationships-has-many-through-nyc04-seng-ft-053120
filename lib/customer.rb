9eaa650cf4cb0be0305c80096df4ecc1a070fbb5
kceron <karemiceron@gmail.com>
2020-06-28 00:15:11 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,39 @@
 class Customer
-  
+    attr_reader :name, :age 
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name 
+        @age = age 
+
+        @@all << self 
+    end
+
+    def self.all
+        @@all
+    end
+
+    #Meal.new(waiter, customer, total, tip)
+    def new_meal(waiter, total, tip = 0)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    #to get a specific customer meals
+    def meals
+        Meal.all.select do |meal|
+            meal.customer == self 
+        end
+    end
+
+    #to get a specid customer waiters
+    def waiters
+        meals.map do |meal|
+          meal.waiter
+        end
+      end
+
+      # to find the name of his last waiter
+      #terrance.waiters.last.name #=> "Yomi"
+      
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,35 @@
+require 'pry'
 class Waiter
+    attr_accessor :name, :years_experience
 
+    @@all = []
+
+    def initialize(name, years_experience)
+        @name = name
+        @years_experience = years_experience
+
+        @@all << self 
+    end
+
+    def self.all
+        @@all 
+    end
+
+    #Meal.new(waiter, customer, total, tip)
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end
+
+    #to get all the meals they have served
+    def meals
+        Meal.all.select { |meal| meal.waiter == self }
+    end
+
+    #To find the best tipper
+    def best_tipper
+        best_tipped_meal = meals.max do |meal_a , meal_b|
+            meal_a.tip <=> meal_b.tip 
+        end
+        best_tipped_meal.customer 
+    end
 end
