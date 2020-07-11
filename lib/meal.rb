3c24a2251877fb895911d6a12013f7b32062a816
chrisvsanchez <chrisvsanchez@gmail.com>
2020-07-11 00:07:46 -0400

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,21 @@
 class Meal
+    attr_accessor :waiter, :customer, :total, :tip
+
+@@all = []
+
+def initialize(waiter, customer, total, tip)
+@waiter = waiter
+@customer = customer
+@total = total
+@tip = tip
+@@all << self
+end
+    def self.find_customer_by_name(name)
+        Meal.all.find{|mean| meal.customer == name}
+    end
+
+def self.all
+    @@all
+end
 
 end
